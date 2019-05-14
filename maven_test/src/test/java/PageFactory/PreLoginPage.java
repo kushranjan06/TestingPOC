49d5fe64e6c3a18c2f1479afc86b2b78fadde09e
kushranjan06 <34791259+kushranjan06@users.noreply.github.com>
2019-05-14 06:32:22 +0530

    Files added for testing

---
+++ maven_test/src/test/java/PageFactory/PreLoginPage.java
@@ -0,0 +1,17 @@
+package PageFactory;
+
+import BaseClassPkg.BaseClass;
+
+public class PreLoginPage extends BaseClass {
+	
+	public PreLoginPage() {
+		super();
+		
+	}
+	
+	public static void main(String args[]) {
+		PreLoginPage preloginpage = new PreLoginPage();
+		preloginpage.startbrowse();
+	}
+
+}
