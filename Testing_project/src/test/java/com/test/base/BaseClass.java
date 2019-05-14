73a0939120185c24d567a527bf84280bed09bda6
kushranjan06 <34791259+kushranjan06@users.noreply.github.com>
2019-05-14 06:15:19 +0530

    First commit

---
+++ Testing_project/src/test/java/com/test/base/BaseClass.java
@@ -0,0 +1,49 @@
+package com.test.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+
+	public static Properties prop;
+
+	public BaseClass() {
+		prop=new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(
+					"G:\\Zomato\\Zomato_Testing\\src\\test\\java\\com\\test\\config\\config.properties");
+			try {
+				prop.load(fis);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/*BaseClass b=new BaseClass();*/
+	
+	public  void startbrowse() {
+		String brwserName = "chrome";
+		if (brwserName.equals(prop.getProperty("Browser"))) {
+			System.setProperty("webdriver.chrome.driver",
+					"G:\\Selenium tutorial chennai\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get(prop.getProperty("url"));
+
+		} else {
+			System.out.println("\n Did not match from config file");
+		}
+	}
+
+}
---
+++ Testing_project/src/test/java/com/test/pagefactory/PreLoginPage.java
@@ -0,0 +1,23 @@
+package com.test.pagefactory;
+
+import com.test.base.BaseClass;
+
+public class PreLoginPage extends BaseClass {
+	
+	
+	
+	
+	public PreLoginPage() {
+		super();
+		
+	}
+	
+	public static void main(String args[]) {
+		PreLoginPage preloginpage = new PreLoginPage();
+		preloginpage.startbrowse();
+	}
+	
+	
+	
+
+}
